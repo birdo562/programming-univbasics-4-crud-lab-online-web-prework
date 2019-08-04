281fc38f4e52887b31dbc653637a08110ebdc334
attentive-rubyist-8517 <robby.medina562@gmail.com>
2019-08-04 07:51:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ["1", "2", "3"]
-  add_element_to_start_of_array.unshift("0")
+  array=["1", "2", "3"]
+  ("0")
   
 end
 
