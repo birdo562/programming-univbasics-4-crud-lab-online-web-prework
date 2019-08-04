c23ccaf1cf44ce90166d0f6b9647213251fbe511
attentive-rubyist-8517 <robby.medina562@gmail.com>
2019-08-04 07:43:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["a", "b", "c"]
-  add_element_to_end_of_array_of_array<<"d"
+  add_element_to_end_of_array_of_array << "d"
 end
 
 def add_element_to_start_of_array(array, element)
